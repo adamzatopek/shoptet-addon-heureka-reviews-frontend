f540d599c4440d4dedf27c04667b9bf0df9e8d44
adamzatopek <adamzatopek@gmail.com>
2026-05-14 17:53:00 +0200

    Remove import statement for initializeSlider

--- src/footer/heureka-fetch-data.js
+++ src/footer/heureka-fetch-data.js
@@ -1,5 +1,4 @@
 // heureka-reviews.js
-import { initializeSlider } from './heureka-reviews-slider.js';
 $(document).ready(function () {
     var HeurekaCountry = { CZ: 1, SK: 2, HU: 3 };
     var HeurekaReviewsTemplateType = { DEFAULT: 0, STYLE1: 1, STYLE2: 2, STYLE3: 3 };
